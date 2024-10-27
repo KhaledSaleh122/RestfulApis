c5c944d2d9e001dd3e1d766caf862eb5e60f3d14
KhaledSaleh122 <voxagolde2003@gmail.com>
2024-10-27 15:20:42 +0200

    Create topic entity

---
+++ Restfulapis-Domain/Entities/Topic.cs
@@ -0,0 +1,9 @@
+﻿namespace Restfulapis_Domain.Entities
+{
+    public class Topic
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
