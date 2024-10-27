8b766477e30ebf25a34a9f0df9cb357152548171
KhaledSaleh122 <voxagolde2003@gmail.com>
2024-10-27 13:03:40 +0200

    Create user repository interface

---
+++ Restfulapis-Domain/Abstractions/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using Restfulapis_Domain.Entities;
+
+namespace Restfulapis_Domain.Abstractions
+{
+    public interface IUserRepository
+    {
+        public Task<User> FindByNameAsync(string name);
+        public Task<bool> CheckPasswordSignInAsync(User user, string password);
+    }
+}
