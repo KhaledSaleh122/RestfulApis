5104114c31068a4780c0e96de435ca868d1952d2
KhaledSaleh122 <voxagolde2003@gmail.com>
2024-10-27 15:53:55 +0200

    Implement create topic handler

---
+++ RestfulApis-Application/Topic/TopicDto.cs
@@ -0,0 +1,8 @@
+﻿namespace RestfulApis_Application.TopicSpace
+{
+    public class TopicDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ RestfulApis-Application/Topic/TopicHandlers.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Restfulapis_Domain.Abstractions;
+using Restfulapis_Domain.Entities;
+
+namespace RestfulApis_Application.TopicSpace
+{
+    public class TopicHandlers
+    {
+        private readonly ITopicRepository _topicRepository;
+        private readonly IMapper _mapper;
+
+        public TopicHandlers(ITopicRepository topicRepository)
+        {
+            _topicRepository = topicRepository ?? throw new ArgumentNullException(nameof(topicRepository));
+            var configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<TopicDto, Topic>();
+            });
+            _mapper = configuration.CreateMapper();
+        }
+
+        public async Task<Result<Topic>> CreateTopicHandler(TopicDto topicDto) {
+            var topic = _mapper.Map<Topic>(topicDto);
+            await _topicRepository.CreateTopicAsync(topic);
+            return new Result<Topic>(topic);
+        }
+    }
+}
