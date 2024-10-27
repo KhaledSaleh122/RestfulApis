44a00c0cc3e80d55b88778dca63b13815ac86e8e
KhaledSaleh122 <voxagolde2003@gmail.com>
2024-10-27 15:10:00 +0200

    Create Authentication controller

---
+++ AuthenticationApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestfulApis_Application.User;
+using Restfulapis_Domain.Abstractions;
+
+namespace RestfulApis_Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly ITokenService _tokenService;
+
+        public AuthenticationController(IUserRepository userRepository, ITokenService tokenService)
+        {
+            _userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
+            _tokenService = tokenService ?? throw new ArgumentNullException(nameof(tokenService));
+        }
+
+        [HttpPost("/login")]
+        public async Task<IActionResult> SignIn(UserDto user) {
+            var command = new UserCommands(_userRepository, _tokenService);
+            var result = await command.SignInHandler(user);
+            if (!result.IsSuccess) {
+                return StatusCode(result.ErrorResult!.StatusCode, result.ErrorResult);
+            }
+            return Ok(result.Value);
+        }
+    }
+}
