285f50c4c196a242dfbda069705a09248296f5d8
KhaledSaleh122 <voxagolde2003@gmail.com>
2024-10-27 16:00:22 +0200

    Create topic controller

---
+++ DataInsertApi/Controllers/TopicController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestfulApis_Application.TopicSpace;
+using Restfulapis_Domain.Abstractions;
+
+namespace DataInsertApi.Controllers
+{
+    [ApiController]
+    [Route("api/topics")]
+    public class TopicController : ControllerBase
+    {
+        private readonly ITopicRepository _topicRepository;
+
+        public TopicController(ITopicRepository topicRepository)
+        {
+            _topicRepository = topicRepository;
+        }
+
+        public async Task<IActionResult> CreateTopic(TopicDto topic) {
+            var handler = new TopicHandlers(_topicRepository);
+            var result = await handler.CreateTopicHandler(topic);
+            if (!result.IsSuccess)
+            {
+                return StatusCode(result.ErrorResult!.StatusCode, result.ErrorResult);
+            }
+            return Ok(result.Value);
+        }
+    }
+}
