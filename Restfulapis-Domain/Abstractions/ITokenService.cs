e0cd7856c18fa0441727823a6e619dce1b792074
KhaledSaleh122 <voxagolde2003@gmail.com>
2024-10-27 15:04:00 +0200

    Create token service interface

---
+++ Restfulapis-Domain/Abstractions/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using Restfulapis_Domain.Entities;
+
+namespace Restfulapis_Domain.Abstractions
+{
+    public interface ITokenService
+    {
+        public string GenerateUserToken(User user);
+    }
+}
