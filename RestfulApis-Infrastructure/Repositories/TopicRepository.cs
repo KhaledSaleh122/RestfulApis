02b9e683b3a3911faa9a465737e25e5a9289d504
KhaledSaleh122 <voxagolde2003@gmail.com>
2024-10-27 15:29:38 +0200

    Implement topic interface

---
+++ RestfulApis-Infrastructure/Repositories/TopicRepository.cs
@@ -0,0 +1,18 @@
+﻿using Restfulapis_Domain.Abstractions;
+using Restfulapis_Domain.Entities;
+
+namespace RestfulApis_Infrastructure.Repositories
+{
+    public class TopicRepository : ITopicRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public TopicRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+        public async Task CreateTopic(Topic topic)
+        {
+            await _dbContext.Topics.AddAsync(topic);
+        }
+    }
+}
