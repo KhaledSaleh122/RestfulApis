2386ce66d805c4c506eec887412c6e6dd10863d2
KhaledSaleh122 <voxagolde2003@gmail.com>
2024-10-27 15:22:41 +0200

    Create topic interface

---
+++ Restfulapis-Domain/Abstractions/ITopicRepository.cs
@@ -0,0 +1,9 @@
+﻿using Restfulapis_Domain.Entities;
+
+namespace Restfulapis_Domain.Abstractions
+{
+    public interface ITopicRepository
+    {
+        public Task CreateTopic(Topic topic);
+    }
+}
