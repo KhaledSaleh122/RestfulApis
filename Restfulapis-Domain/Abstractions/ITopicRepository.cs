011cfb3478b69cdaf6c00bfccadc66691d2a4244
KhaledSaleh122 <voxagolde2003@gmail.com>
2024-10-27 17:06:58 +0200

    Add Find topic/topics to topic interface

--- Restfulapis-Domain/Abstractions/ITopicRepository.cs
+++ Restfulapis-Domain/Abstractions/ITopicRepository.cs
@@ -5,5 +5,7 @@ namespace Restfulapis_Domain.Abstractions
     public interface ITopicRepository
     {
         public Task CreateTopicAsync(Topic topic);
+        public Task<Topic?> FindTopicByIdAsync(int id);
+        public Task<(IEnumerable<Topic> Topics, int TotalRecords)> FindTopics(int page = 1,int pageSize=10);
     }
 }
