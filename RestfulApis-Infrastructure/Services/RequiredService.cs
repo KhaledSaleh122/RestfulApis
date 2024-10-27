1c2ae97087d8d561f4a3a1ef3db88b3d4f04210e
KhaledSaleh122 <voxagolde2003@gmail.com>
2024-10-27 15:09:23 +0200

    Add required services

---
+++ RestfulApis-Infrastructure/Services/RequiredService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Restfulapis_Domain.Abstractions;
+using RestfulApis_Infrastructure.Repositories;
+
+namespace RestfulApis_Infrastructure.Services
+{
+    public static class RequiredService
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<ITokenService, TokenService>();
+            return services;
+        }
+    }
+}
