c7516c2858f37cac8874998f63965a7550dd170b
KhaledSaleh122 <voxagolde2003@gmail.com>
2024-10-27 12:41:33 +0200

    Create dbcontext service

---
+++ RestfulApis-Infrastructure/Services/DbContextService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RestfulApis_Infrastructure.Services
+{
+    public static class DbContextService
+    {
+        public static IServiceCollection AddDBContextConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(
+                options =>
+                {
+                    options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+                    options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+                },
+                ServiceLifetime.Scoped
+            );
+            return services;
+        }
+    }
+}
