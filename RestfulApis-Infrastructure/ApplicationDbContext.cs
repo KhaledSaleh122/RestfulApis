e763315d1f6d7c44020cb5a287539bbcda1059f4
KhaledSaleh122 <voxagolde2003@gmail.com>
2024-10-27 12:53:28 +0200

    Update dbcontext

--- RestfulApis-Infrastructure/ApplicationDbContext.cs
+++ RestfulApis-Infrastructure/ApplicationDbContext.cs
@@ -6,7 +6,14 @@ namespace RestfulApis_Infrastructure
     public class ApplicationDbContext : DbContext
     {
         public DbSet<User> Users { get; set; }
-
+        public ApplicationDbContext(DbContextOptions options)
+        : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
