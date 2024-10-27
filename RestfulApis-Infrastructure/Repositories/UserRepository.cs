81ac278bf67bbc1f9f1a80c3a30120fa7e8d4d4e
KhaledSaleh122 <voxagolde2003@gmail.com>
2024-10-27 13:09:47 +0200

    Implement user interface

---
+++ RestfulApis-Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restfulapis_Domain.Abstractions;
+using Restfulapis_Domain.Entities;
+
+namespace RestfulApis_Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public UserRepository(ApplicationDbContext dbContext) {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+        public Task<bool> CheckPasswordSignInAsync(User user, string password)
+        {
+            return Task.FromResult(user.Password == password);
+        }
+
+        public Task<User?> FindByNameAsync(string name)
+        {
+            return _dbContext.Users.FirstOrDefaultAsync(user => user.Username == name);
+        }
+    }
+}
--- Restfulapis-Domain/Abstractions/IUserRepository.cs
+++ Restfulapis-Domain/Abstractions/IUserRepository.cs
@@ -4,7 +4,7 @@ namespace Restfulapis_Domain.Abstractions
 {
     public interface IUserRepository
     {
-        public Task<User> FindByNameAsync(string name);
+        public Task<User?> FindByNameAsync(string name);
         public Task<bool> CheckPasswordSignInAsync(User user, string password);
     }
 }
